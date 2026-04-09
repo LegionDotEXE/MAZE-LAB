d4d91fc1a8f3bea6fe70f2c5db02e8d22f91a7b6
Robert Stager <queenbee.stager@gmail.com>
2026-04-09 15:22:43 -0700

    Added changes from main into this branch and allowed for miniGame and Shop scene to run in parallel.

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -8,7 +8,10 @@ class Load extends Phaser.Scene{
     }
 
     create(){
-        this.scene.start('ShopScene')
+
+        //use .launch to start your scenes so they can run in parallel and not interrupt each other - Robert
+        this.scene.launch('ShopScene')
+        this.scene.launch('MiniGame')
     }
 
 }
