0537f15cdbb2a4866ba252e07e6e30ce8e220b3d
Robert Stager <queenbee.stager@gmail.com>
2026-04-10 11:10:45 -0700

    Cahnges scene Launch Order

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -22,9 +22,9 @@ class Load extends Phaser.Scene{
     create(){
         //use .launch to start your scenes so they can run in parallel and not interrupt each other - Robert
         this.scene.launch('GameManager')
-        this.scene.launch('ShopScene')
-        this.scene.launch('MiniGame')
         this.scene.launch('mazeScene')
+        this.scene.launch('MiniGame')
+        this.scene.launch('ShopScene')
     }
 
 }
