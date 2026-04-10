660cd5d0bf5b8b3d4cbfe416646e388fd5b0048c
mydoot <buaqur@gmail.com>
2026-04-10 11:06:17 -0700

    Maze updates

--- src/Scenes/Maze.js
+++ src/Scenes/Maze.js
@@ -1,62 +1,4 @@
 //LLMs were used for a few function, view the chat log here: https://gemini.google.com/share/fe7fd7289a33
-class HealthBar {
-    constructor (scene, x, y)
-    {
-        this.bar = new Phaser.GameObjects.Graphics(scene);
-
-        this.x = x;
-        this.y = y;
-        this.value = 100;
-        this.maxHealthbarSize = 246; 
-        this.p = this.maxHealthbarSize / 100;
-
-        this.draw();
-
-        scene.add.existing(this.bar);
-    }
-
-    decrease (amount)
-    {
-        this.value -= amount;
-
-        if (this.value < 0)
-        {
-            this.value = 0;
-        }
-
-        this.draw();
-
-        return (this.value === 0);
-    }
-
-    draw ()
-    {
-        this.bar.clear();
-
-        //  BG
-        this.bar.fillStyle(0x000000);
-        this.bar.fillRect(this.x, this.y, 250, 16);
-
-        //  Health
-
-        this.bar.fillStyle(0xffffff);
-        this.bar.fillRect(this.x + 2, this.y + 2, this.maxHealthbarSize, 12);
-
-        if (this.value < 30)
-        {
-            this.bar.fillStyle(0xff0000);
-        }
-        else
-        {
-            this.bar.fillStyle(0x00ff00);
-        }
-
-        var d = Math.floor(this.p * this.value);
-
-        this.bar.fillRect(this.x + 2, this.y + 2, d, 12);
-    }
-
-}
 
 class HealthBar {
     constructor (scene, x, y)
@@ -193,7 +135,7 @@ class Maze extends Phaser.Scene {
         this.startingLocation2 = { x: this.tileXtoWorld(10), y: this.tileYtoWorld(13) }
 
         // create lobster sprite
-        my.sprite.lobster = new Animal(this, this.startingLocation.x, this.startingLocation.y, "Lobster", null, this.map).setOrigin(0, 0);
+        my.sprite.lobster = new Animal(this, this.startingLocation2.x, this.startingLocation2.y, "Lobster", null, this.map).setOrigin(0, 0);
 
         this.activeCharacter = my.sprite.lobster;
 
@@ -302,7 +244,7 @@ class Maze extends Phaser.Scene {
                 this.activeCharacter.statemachine.transition("Thinking");
                 //thinking tiles turn themselves off for the characters thinking time + 1.5s in order to prevent the lobster 
                 //from being stuck thinking on the same tile for so long
-                this.time.delayedCall((this.activeCharacter.thinkingTime + 1500), () => { 
+                this.time.delayedCall((this.activeCharacter.thinkingTime + 2000), () => { 
                     tile.properties.THINKING = true;
                 }, null, this);
             }
