46da0446c4b5e8b91d88fb38a0988bfba8e3fac7
Robert Stager <queenbee.stager@gmail.com>
2026-04-17 00:26:35 -0700

    Moved button to the left again

--- src/scenes/mini-game.js
+++ src/scenes/mini-game.js
@@ -13,7 +13,7 @@ class MiniGame extends Phaser.Scene {
 
   create() {  
     this.Maze = game.scene.getScene('mazeScene');
-    this.button = this.add.sprite(700, 300, 'buttonUp').setDepth(0).setInteractive();
+    this.button = this.add.sprite(600, 300, 'buttonUp').setDepth(0).setInteractive();
     
     // lightning effect
     this.lightningEmitter = this.add.particles(0, 0, 'lightning', {
